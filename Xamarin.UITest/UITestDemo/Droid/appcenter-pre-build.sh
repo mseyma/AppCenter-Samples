281e12b56fe93f42ceb15506c80420c71f42ae36
mseyma <eyma@microsoft.com>
2019-07-08 11:20:16 -0700

    comment out mono upgrade

--- Xamarin.UITest/UITestDemo/Droid/appcenter-pre-build.sh
+++ Xamarin.UITest/UITestDemo/Droid/appcenter-pre-build.sh
@@ -10,8 +10,8 @@ echo "checking mono before install --version"
 mono --version
 
 echo "installing mono 5.18.1.28"
-wget https://download.visualstudio.microsoft.com/download/pr/d709da85-c472-437d-9e3d-34d0aece1f1d/1795624d8fe42827f89625fd8553d79b/monoframework-mdk-5.18.1.28.macos10.xamarin.universal.pkg
-sudo installer -pkg "monoframework-mdk-5.18.1.28.macos10.xamarin.universal.pkg" -target /
+#wget https://download.visualstudio.microsoft.com/download/pr/d709da85-c472-437d-9e3d-34d0aece1f1d/1795624d8fe42827f89625fd8553d79b/monoframework-mdk-5.18.1.28.macos10.xamarin.universal.pkg
+#sudo installer -pkg "monoframework-mdk-5.18.1.28.macos10.xamarin.universal.pkg" -target /
 
 echo "checking mono after install --version"
 mono --version
@@ -20,12 +20,12 @@ mono --version
 echo "checking dotnet before install --version"
 dotnet --version
 
-DOTNET_INSTALL_SCRIPT="https://dot.net/v1/dotnet-install.sh"
-curl -s -o "dotnet-install.sh" "$DOTNET_INSTALL_SCRIPT"
-chmod +x ./dotnet-install.sh
+#DOTNET_INSTALL_SCRIPT="https://dot.net/v1/dotnet-install.sh"
+#curl -s -o "dotnet-install.sh" "$DOTNET_INSTALL_SCRIPT"
+#chmod +x ./dotnet-install.sh
 
 # Mono 5.1.18.28 supports 2.2.300/2.1.700
-./dotnet-install.sh --version 2.2.300 -NoPath
+#./dotnet-install.sh --version 2.2.300 -NoPath
 
 echo "checking dotnet before install --version"
 dotnet --version
